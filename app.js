af778b1b69b9ad27a228922de88b9d3e2cb81509
Kapetzu <kasper.laaksonen98@gmail.com>
2026-09-13 15:58:19 +0300

    Fixing login issue possibly with new ENV Variable

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production'
+    //secure: process.env.NODE_ENV === 'production'
+    secure: false
   }
 }));
 
